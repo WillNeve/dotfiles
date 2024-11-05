02cf6048210ff7545fe07728c3db86eab330b0ba
William Neve <williamneve6000@gmail.com>
2024-11-05 16:56:50 +0000

    copy text binds for tmux

--- localista-tmux.sh
+++ localista-tmux.sh
@@ -9,22 +9,22 @@ tmux split-window -h -t $SESSION_NAME:0
 tmux split-window -v -t $SESSION_NAME:0.1
 
 tmux new-window -t $SESSION_NAME:1 -n 'main'
-
 tmux new-window -t $SESSION_NAME:2 -n 'htop'
 
-sleep 0.2
-tmux send-keys -t $SESSION_NAME:0.0 "cd $PROJECT_PATH && npm run emailengine" C-m
-sleep 0.2
-tmux send-keys -t $SESSION_NAME:0.1 "cd $PROJECT_PATH && npm run dev" C-m
-sleep 0.2
-tmux send-keys -t $SESSION_NAME:0.2 "cd $PROJECT_PATH && prisma studio" C-m
-sleep 0.2
-tmux send-keys -t $SESSION_NAME:1 "cd $PROJECT_PATH && git fetch && git status" C-m
-sleep 0.2
+sleep 0.5 # shell startup time
+tmux send-keys -t $SESSION_NAME:0.0 "cd $PROJECT_PATH && clear" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "cd $PROJECT_PATH && clear" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "cd $PROJECT_PATH && clear" C-m
+tmux send-keys -t $SESSION_NAME:1 "cd $PROJECT_PATH && clear" C-m
 tmux send-keys -t $SESSION_NAME:2 "htop" C-m
+sleep 0.3 # cd and clear time
+tmux send-keys -t $SESSION_NAME:0.0 "npm run emailengine" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "npm run dev" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "prisma studio" C-m
+tmux send-keys -t $SESSION_NAME:1 "git fetch && git status" C-m
+sleep 0.2
 
-# Select window 2 and the first pane
 tmux select-window -t $SESSION_NAME:1
 tmux select-pane -t 0
-# Attach to session
+
 tmux attach-session -t $SESSION_NAME
